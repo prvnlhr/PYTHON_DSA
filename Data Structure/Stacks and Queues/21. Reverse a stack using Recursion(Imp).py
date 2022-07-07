fba6cd50f1b33d065453702a513b6b2b01899127
Praveen Lohar <praveenlohar.in@gmail.com>
2022-07-07 19:14:00 +0530

    Stacks and Queue :done 21

---
+++ Data Structure/Stacks and Queues/21. Reverse a stack using Recursion(Imp).py
@@ -0,0 +1,24 @@
+# NOTE:: JUST MUG UP THIS SOLUTION BECAUSE IT IS A TECHNIQUE TO STORE ,
+# THE VALUES IN CALL STACK OF FUNCTION TO REVERSE THE STACK
+# SIMILAR SOLUTION IS USED FOR SORTING A STACK USING RECURSION
+
+
+def insertAtBottom(stack, item):
+    if len(stack) == 0:
+        stack.append(item)
+    else:
+        temp = stack.pop()
+        insertAtBottom(stack, item)
+        stack.append(temp)
+
+
+def reverse(stack):
+    if len(stack) != 0:
+        temp = stack.pop()
+        reverse(stack)
+        insertAtBottom(stack, temp)
+
+
+s = [int(i) for i in input().strip().split()]
+reverse(s)
+print(s)
