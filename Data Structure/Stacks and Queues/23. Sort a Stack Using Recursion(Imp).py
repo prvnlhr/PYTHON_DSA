efa80eeef8d25d000d5a5ff011382ae5de92f227
Praveen Lohar <praveenlohar.in@gmail.com>
2022-07-08 09:21:00 +0530

    Stacks and Queue :done 23

---
+++ Data Structure/Stacks and Queues/23. Sort a Stack Using Recursion(Imp).py
@@ -0,0 +1,21 @@
+def sortInserted(s, ele):
+    # ele > s[-1] this condition is only diff compare to reverse stack sol
+    if len(s) == 0 or ele > s[-1]:
+        s.append(ele)
+        # return
+    else:
+        temp = s.pop()
+        sortInserted(s, ele)
+        s.append(temp)
+
+
+def sortStack(stack):
+    if len(stack) != 0:
+        temp = stack.pop()
+        sortStack(stack)
+        sortInserted(stack, temp)
+
+
+s = [int(i) for i in input().strip().split()]
+sortStack(s)
+print(s)
